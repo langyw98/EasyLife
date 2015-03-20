72c09e957503a37217f2b77c144b96bc3e5fa02f
unknown <yang.liu@liuy-PC.jv.jb-aptech.com.cn>
2015-03-20 13:17:38 +0800

    注册功能

--- src/cn/bdqn/life/activity/LoginActivity.java
+++ src/cn/bdqn/life/activity/LoginActivity.java
@@ -1,8 +1,11 @@
 package cn.bdqn.life.activity;
 
-import cn.bdqn.life.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import cn.bdqn.life.R;
 
 public class LoginActivity extends Activity {
 	@Override
@@ -10,5 +13,19 @@ public class LoginActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+	}
+	public void BtnOnClick(View view){
+		Intent intent = new Intent();
+		switch(view.getId()){
+		case R.id.btn_login:
+			break;
+		case R.id.btn_regist:
+			intent.setClass(this, cn.bdqn.life.activity.RegActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.tv_problem:
+			break;
+		}
+		
 	}
 }
--- src/cn/bdqn/life/activity/RegActivity.java
+++ src/cn/bdqn/life/activity/RegActivity.java
@@ -1,14 +1,106 @@
 package cn.bdqn.life.activity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import cn.bdqn.life.R;
+import cn.bdqn.life.net.HttpConnection;
+import cn.bdqn.life.net.URLParam;
+import cn.bdqn.life.net.URLProtocol;
 
 public class RegActivity extends Activity {
-
+	private static final int MSG_CONNECT_FAILED = 0;
+	private static final int MSG_ACCOUNT_EXIST = 1;
+	private static final int MSG_REGIST_SUCCESS = 2;
+	
+	private EditText et_account;
+	private EditText et_password;
+	private EditText et_confirm;
+	
+	private Handler handler = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			switch(msg.what){
+			case MSG_CONNECT_FAILED:
+			case MSG_ACCOUNT_EXIST:
+				Toast.makeText(RegActivity.this, "注册失败", Toast.LENGTH_LONG).show();
+				break;
+			case MSG_REGIST_SUCCESS:
+				break;
+			}
+		};
+	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_regist);
+		init();
 	}
 	
+	private void init(){
+		et_account = (EditText) findViewById(R.id.et_account);
+		et_password = (EditText) findViewById(R.id.et_password);
+		et_confirm = (EditText) findViewById(R.id.et_confirm);
+	}
+	
+	public void BtnOnclick(View view){
+		switch(view.getId()){
+		case R.id.btn_regist:
+			String strAccount = et_account.getText().toString().trim();
+			String strPassword = et_password.getText().toString().trim();
+			String strConfirm = et_confirm.getText().toString().trim();
+			//注册用户名为空
+			if(strAccount == null || strAccount.equals("")){
+				Toast.makeText(this, "用户名不能为空，请填写用户名", Toast.LENGTH_LONG).show();
+			}//输入密码为空
+			else if(strPassword == null || strPassword.equals("")){
+				Toast.makeText(this, "密码不能为空，请填写密码", Toast.LENGTH_LONG).show();
+			}//两次输入的密码不相同
+			else if(strConfirm == null || !strConfirm.equals(strPassword)){
+				Toast.makeText(this, "两次输入的密码不一致，请确认并修改", Toast.LENGTH_LONG).show();
+			}
+			//调用注册方法
+			Regist(strAccount, strPassword);
+			break;
+		}
+	}
+	
+	private void Regist(final String strAccount, final String strPassword){
+		new Thread(){
+			public void run() {
+				URLParam param = new URLParam(null);
+				param.addParam("name", strAccount);
+				param.addParam("password", strPassword);
+				param.addParam("cmd", URLProtocol.CMD_REGISTER);
+				String jsonStr = HttpConnection.httpGet(URLProtocol.ROOT, param);
+				//与服务器连接失败
+				if(jsonStr == null){
+					handler.sendEmptyMessage(MSG_CONNECT_FAILED);
+				}
+				try {
+					JSONObject json = new JSONObject(jsonStr);
+					int code = json.getInt("code");
+					if (code == 0) {
+						String uid = json.getString("uid");
+						Log.i("RegActivity","uid is "+ uid);
+						handler.sendEmptyMessage(MSG_REGIST_SUCCESS);
+						return;
+					}
+					handler.sendEmptyMessage(MSG_ACCOUNT_EXIST);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			};
+		}.start();
+	}
+	
+
 }
