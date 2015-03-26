933b284237001f34da552e5e6198fb2174b49573
unknown <yang.liu@liuy-PC.jv.jb-aptech.com.cn>
2015-03-26 17:41:18 +0800

    MainFragment界面完成

--- src/cn/bdqn/life/activity/MainActivity.java
+++ src/cn/bdqn/life/activity/MainActivity.java
@@ -3,14 +3,18 @@ package cn.bdqn.life.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import cn.bdqn.life.R;
+import cn.bdqn.life.fragment.FavorFragment;
 import cn.bdqn.life.fragment.MainFragment;
+import cn.bdqn.life.fragment.RecommendFragment;
 import cn.bdqn.life.fragment.UserInfoFragment;
 
 public class MainActivity extends FragmentActivity {
@@ -19,22 +23,73 @@ public class MainActivity extends FragmentActivity {
 	private FragmentPagerAdapter mAdapter;
 	private List<Fragment> mFragments;
 	
+	private LinearLayout lTabMain;
+	private LinearLayout lTabRecommend;
+	private LinearLayout lTabFavor;
+	private LinearLayout lTabUserInfo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
+		initEvent();
+	}
+	
+	private void initEvent(){
+		lTabMain.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		lTabRecommend.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});		
+		lTabFavor.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		lTabUserInfo.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 	
 	private void initView() {
 		// TODO Auto-generated method stub
+		
+		lTabMain = (LinearLayout) findViewById(R.id.tab_1);
+		lTabRecommend = (LinearLayout) findViewById(R.id.tab_2);
+		lTabFavor = (LinearLayout) findViewById(R.id.tab_3);
+		lTabUserInfo = (LinearLayout) findViewById(R.id.tab_4);
+		
 		viewPager = (ViewPager) findViewById(R.id.view_pager);
 		Fragment mainFragment = new MainFragment();
+		Fragment recommendFragment = new RecommendFragment();
+		Fragment favorFragment = new FavorFragment();
 		Fragment userInfoFragment = new UserInfoFragment();
 		
 		mFragments = new ArrayList<Fragment>();
 		mFragments.add(mainFragment);
+		mFragments.add(recommendFragment);
+		mFragments.add(favorFragment);
 		mFragments.add(userInfoFragment);
 		
 		mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
--- src/cn/bdqn/life/fragment/RecommendFragment.java
+++ src/cn/bdqn/life/fragment/RecommendFragment.java
@@ -6,21 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
-
-
-public class MainFragment extends Fragment {
-	private FrameLayout fFilm;
-	private FrameLayout fExhibition;
-	private FrameLayout fFood;
-	private FrameLayout fShow;
-	
+public class RecommendFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		return inflater.inflate(R.layout.fragment_main, null);
+		return inflater.inflate(R.layout.fragment_recommend, null);
 	}
-	
 }
