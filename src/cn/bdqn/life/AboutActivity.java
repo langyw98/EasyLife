e5a6a492b02454784fddaeabe60bb2616d624991
unknown <yang.liu@liuy-PC.jv.jb-aptech.com.cn>
2015-05-29 13:58:30 +0800

    精简代码

--- src/cn/bdqn/life/AboutActivity.java
+++ src/cn/bdqn/life/AboutActivity.java
@@ -1,13 +1,14 @@
 package cn.bdqn.life;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 /**¹ØÓÚ*/
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
+		setContentView(R.layout.activity_about);
 	}
 }
